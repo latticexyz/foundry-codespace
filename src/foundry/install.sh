0d00d9883bbcc25c8f1662fa43152bd9dac77d4a
alvrs <alvarius@lattice.xyz>
2024-05-19 15:30:53 +0100

    move binaries to user path

--- src/foundry/install.sh
+++ src/foundry/install.sh
@@ -11,3 +11,7 @@ curl -L https://foundry.paradigm.xyz | bash
 
 echo "Running foundryup"
 /root/.foundry/bin/foundryup
+
+# Move foundry binaries to /usr/local/bin
+echo "Moving foundry binaries to /usr/local/bin"
+mv /root/.foundry/bin/* /usr/local/bin/
