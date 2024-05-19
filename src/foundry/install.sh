94a6eef5dd28a08750d9b4eeef5de78ba51d5e07
alvrs <alvarius@lattice.xyz>
2024-05-19 14:56:47 +0100

    fix ubuntu and debian tests

--- src/foundry/install.sh
+++ src/foundry/install.sh
@@ -2,7 +2,12 @@
 set -e
 
 echo "Activating feature 'foundry'"
+
+echo "Installing dependencies"
+apt-get -y update; apt-get -y install curl git
+
 echo "Installing foundryup"
 curl -L https://foundry.paradigm.xyz | bash
+
 echo "Running foundryup"
 /root/.foundry/bin/foundryup
